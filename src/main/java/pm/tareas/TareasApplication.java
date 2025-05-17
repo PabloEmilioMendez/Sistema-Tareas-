f3a6bf1c1bcb7fbce2c32d3268f95cc5ee505475
Pablo Mendez <pmendezpuentes@gmail.com>
2025-05-16 22:12:32 -0500

    Se agregaron las funcionalidades de listar, agregar, modificar, eliminar tarea y limpar fomulario

--- src/main/java/pm/tareas/TareasApplication.java
+++ src/main/java/pm/tareas/TareasApplication.java
@@ -1,13 +1,16 @@
 package pm.tareas;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pm.tareas.presentacion.SistemaTareasFx;
 
 @SpringBootApplication
 public class TareasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TareasApplication.class, args);
+		//SpringApplication.run(TareasApplication.class, args);
+		Application.launch(SistemaTareasFx.class, args);
 	}
 
 }
